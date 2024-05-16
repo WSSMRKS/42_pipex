3f5a458c28a424de44681e03527e6ae51ea09e77
Markus Weiss <markusweiss.mw@gmail.com>
2024-05-16 12:58:31 +0200

    basic structure makes sense. Next step: execute commands with access

--- pipex.c
+++ pipex.c
@@ -6,29 +6,92 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 09:54:20 by maweiss           #+#    #+#             */
-/*   Updated: 2024/05/16 10:19:34 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/05/16 12:56:00 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv, char **envp)
+int ft_final_child(t_pipex *pipex)
+{
+	int	pid;
+	
+	pid = fork();
+	if (pid < 0)
+	{
+		strerror(10);
+		exit(10);
+	}
+	else if (pid == 0)
+	{
+		pipex->argc = pipex->argc - 2;
+		ft_child_n(pipex);
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+		//execute last command, write to file;
+		
+	}
+	if (ft_strncmp(pipex->argv[1], "here_doc", 8 == 0))
+		ft_here_doc();
+	
+}
+
+char **ft_grab_envp(char **envp)
 {
 	int	i;
 
 	i = 0;
+	while(envp && envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			break ;
+		i++;
+	}
+	return (ft_split(envp[i], ':'));
+}
 
+
+void	ft_init_env(t_pipex pipex, int *argc, char **argv, char **envp)
+{
+	pipex.envp = envp;
+	pipex.argv = argv;
+	pipex.argc = argc;
+	pipex.path = ft_grab_envp(envp);
+}
+
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		i;
+	int		pid1;
+	t_pipex	pipex;
+	
+
+	i = 0;
 	if (argc < 5)
-	{
-		ft_printf_err()
-	}
-
-	if (ft_strncmp(argv[1], "here_doc", 8 != 0)
-		regular execution
+		ft_printf_err("Error: Wrong number of arguments");
 	else
-		(heredoc execution)
-	while (i < argc)
 	{
-		check number of childprocesses
-	}
+		// regular execution
+		// create child process. (last 2 args get first (flip the line.)
+		// incorporate PATH
+		ft_init_env(pipex, &argc, argv, envp);
+		if (pipe(pipex.fd) == -1)
+		{
+			strerror(32);
+			return (32);
+		}
+		pid1 = fork();
+		if (pid1 < 0)
+		{
+			strerror(10);
+			return (10);
+		}
+		else if (pid1 == 0)
+			ft_final_child(&pipex);
+		else
+			waitpid(pid1, NULL, 0);
+	}		
 }
--- pipex.h
+++ pipex.h
@@ -6,9 +6,19 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:03:45 by maweiss           #+#    #+#             */
-/*   Updated: 2024/05/16 10:18:53 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/05/16 12:55:33 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+typedef struct s_pipex {
+	char	**path;
+	char	**envp;
+	char	**argv;
+	int		*argc;
+	int		fd[2];
+}				t_pipex;
