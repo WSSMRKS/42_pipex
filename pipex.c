acc2f93e27924ffbbe550ee56afe35ce1ecdb7b4
Markus Weiss <markusweiss.mw@gmail.com>
2024-05-16 10:28:09 +0200

    started coding

---
+++ pipex.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 09:54:20 by maweiss           #+#    #+#             */
+/*   Updated: 2024/05/16 10:19:34 by maweiss          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	i;
+
+	i = 0;
+
+	if (argc < 5)
+	{
+		ft_printf_err()
+	}
+
+	if (ft_strncmp(argv[1], "here_doc", 8 != 0)
+		regular execution
+	else
+		(heredoc execution)
+	while (i < argc)
+	{
+		check number of childprocesses
+	}
+}
