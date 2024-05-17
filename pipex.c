5279fe3f9251fa47cc8cb9b0fc941351e2ac5671
Markus Weiss <maweiss@c4a8c1.42berlin.de>
2024-05-17 12:06:03 +0200

    created all the child processes, hope this works

--- pipex.c
+++ pipex.c
@@ -6,44 +6,145 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 09:54:20 by maweiss           #+#    #+#             */
-/*   Updated: 2024/05/16 12:56:00 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/05/17 12:02:18 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int ft_final_child(t_pipex *pipex)
+int	ft_cmd_last(t_pipex *pipex, int argc_l)
 {
-	int	pid;
-	
-	pid = fork();
-	if (pid < 0)
+	int		i;
+	char	*ppath;
+	char	**cmd;
+	int		outfile;
+
+	i = 0;
+	cmd = ft_split(pipex->argv[argc_l - 2], ' ');
+	ft_lstadd_back(pipex->free, ft_pipex_lstnew(cmd, 2));
+	while (pipex->path[i])
+	{
+		ppath = ft_strjoin(pipex->path[i], cmd[0]);
+		if (access(ppath, X_OK) == 0)
+			break ;
+		i++;
+	}
+	ft_lstadd_back(pipex->free, ft_pipex_lstnew(ppath, 1));
+	if (pipex->here_doc == 1)
+		outfile = open(pipex->argv[1], O_APPEND | O_CREAT, 0777);
+	else
+		outfile = open(pipex->argv[1], O_TRUNC | O_CREAT, 0777);
+	dup2(outfile, STDIN_FILENO);
+	dup2(pipex->pipe1[1], STDOUT_FILENO);
+	close(outfile);
+	close(pipex->pipe1[0]);
+	execve(ppath, &cmd[1], pipex->envp);
+}
+
+t_list	*ft_pipex_lstnew(void *cont, int type)
+{
+	t_list	*test;
+
+	test = malloc(sizeof(t_list));
+	if (!test)
+		return (NULL);
+	test->cont = cont;
+	test->type = type;
+	test->next = NULL;
+	return (test);
+}
+
+
+int	ft_cmd_first(t_pipex *pipex, int argc_l)
+{
+	int		i;
+	char	*ppath;
+	char	**cmd;
+	int		infile;
+
+	i = 0;
+	cmd = ft_split(pipex->argv[*pipex->argc], ' ');
+	ft_lstadd_back(pipex->free, ft_pipex_lstnew(cmd, 2));
+	while (pipex->path[i])
+	{
+		ppath = ft_strjoin(pipex->path[i], cmd[0]);
+		if (access(ppath, X_OK) == 0)
+			break ;
+		i++;
+	}
+	ft_lstadd_back(pipex->free, ft_pipex_lstnew(ppath, 1));
+	if (ft_strncmp(pipex->argv[1], "here_doc", 8 == 0))
+		ft_here_doc();
+	else
+	{
+		infile = open(pipex->argv[1], O_RDONLY);
+		dup2(infile, STDIN_FILENO);
+		dup2(pipex->pipe1[1], STDOUT_FILENO);
+		close(infile);
+		close(pipex->pipe1[0]);
+		execve(ppath, &cmd[1], pipex->envp);
+	}
+}
+
+int	ft_child(t_pipex *pipex)
+{
+	int	pid_n;
+	int	argc_l;
+
+	argc_l = pipex->argc;
+	if (argc_l <= 3)
+		ft_cmd_first(pipex, argc_l);
+	else
+	{
+		pid_n = fork();
+		if (pid_n < 0)
+		{
+			strerror(10);
+			exit(10);
+		}
+		else if (pid_n == 0)
+		{
+			pipex->argc = pipex->argc - 1;
+			ft_child_n(pipex);
+		}
+		else
+		{
+			waitpid(pid_n, NULL, 0);
+			ft_cmd_n();
+		}
+	}
+}
+
+int	ft_parent_process(t_pipex *pipex)
+{
+	int	pid_f;
+	int	argc_l;
+
+	argc_l = pipex->argc;
+	pid_f = fork();
+	if (pid_f < 0)
 	{
 		strerror(10);
 		exit(10);
 	}
-	else if (pid == 0)
+	else if (pid_f == 0)
 	{
 		pipex->argc = pipex->argc - 2;
 		ft_child_n(pipex);
 	}
 	else
 	{
-		waitpid(pid, NULL, 0);
-		//execute last command, write to file;
-		
+		waitpid(pid_f, NULL, 0);
+		ft_cmd_last(pipex, argc_l);
 	}
-	if (ft_strncmp(pipex->argv[1], "here_doc", 8 == 0))
-		ft_here_doc();
-	
 }
 
-char **ft_grab_envp(char **envp)
+char	**ft_grab_envp(char **envp)
 {
 	int	i;
 
 	i = 0;
-	while(envp && envp[i])
+	while (envp && envp[i])
 	{
 		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
 			break ;
@@ -52,46 +153,48 @@ char **ft_grab_envp(char **envp)
 	return (ft_split(envp[i], ':'));
 }
 
-
 void	ft_init_env(t_pipex pipex, int *argc, char **argv, char **envp)
 {
 	pipex.envp = envp;
 	pipex.argv = argv;
 	pipex.argc = argc;
 	pipex.path = ft_grab_envp(envp);
+	pipex.free = ft_pipex_lstnew(NULL, 0);
 }
-
 
 int	main(int argc, char **argv, char **envp)
 {
 	int		i;
-	int		pid1;
+	int		pid_m;
 	t_pipex	pipex;
-	
 
 	i = 0;
 	if (argc < 5)
 		ft_printf_err("Error: Wrong number of arguments");
 	else
 	{
-		// regular execution
-		// create child process. (last 2 args get first (flip the line.)
-		// incorporate PATH
+		if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+			pipex.here_doc = 1;
 		ft_init_env(pipex, &argc, argv, envp);
-		if (pipe(pipex.fd) == -1)
+		if (pipe(pipex.pipe0) == -1)
 		{
 			strerror(32);
 			return (32);
 		}
-		pid1 = fork();
-		if (pid1 < 0)
+		if (pipe(pipex.pipe1) == -1)
+		{
+			strerror(32);
+			return (32);
+		}
+		pid_m = fork();
+		if (pid_m < 0)
 		{
 			strerror(10);
 			return (10);
 		}
-		else if (pid1 == 0)
-			ft_final_child(&pipex);
+		else if (pid_m == 0)
+			ft_parent_process(&pipex);
 		else
-			waitpid(pid1, NULL, 0);
-	}		
+			waitpid(pid_m, NULL, 0);
+	}
 }
--- pipex.h
+++ pipex.h
@@ -6,19 +6,105 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:03:45 by maweiss           #+#    #+#             */
-/*   Updated: 2024/05/16 12:55:33 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/05/17 11:55:24 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <limits.h>
+# include <ctype.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+
+# endif
+# ifndef MAX_FD
+#  define MAX_FD 1048
+
+# endif
+
+typedef struct s_list
+{
+	void			*cont;
+	int				type; // 1 = string, 2 = 2d-array, 0 = empty
+	struct s_list	*next;
+}					t_list;
 
 typedef struct s_pipex {
 	char	**path;
 	char	**envp;
 	char	**argv;
 	int		*argc;
-	int		fd[2];
+	int		pipe0[2];
+	int		pipe1[2];
+	t_list	*free;
+	int		here_doc;
 }				t_pipex;
+
+int		ft_atoi(const char *nptr);
+long	ft_atol(const char *nptr);
+int		ft_countwords(char const *str, char c);
+void	*ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+char	*ft_itoa(int nb);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *src);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+t_list	*ft_lstnew(void *cont);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+int		ft_printf(const char *s, ...);
+int		ft_printf_err(const char *s, ...);
+int		ft_pnb_b_fd(long nbr, char *base, int fd, int negp);
+int		ft_pnb_b_fd_s(int nbr, char *base, int fd, int negp);
+int		ft_putstr_fd_ret(char *s, int fd);
+int		ft_putchar_fd_ret(char c, int fd);
+char	*ft_get_next_line(int fd);
+void	ft_putstr_non_printable(char *str, size_t n);
+int		ft_abs(int a);
+
+#endif
