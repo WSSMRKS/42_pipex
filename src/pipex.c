5be944b13e78567114db45ecab2fe0fb1772b90f
Markus Weiss <maweiss@c4a8c1.42berlin.de>
2024-05-17 13:11:45 +0200

    time to get it running

--- src/pipex.c
+++ src/pipex.c
@@ -6,40 +6,11 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 09:54:20 by maweiss           #+#    #+#             */
-/*   Updated: 2024/05/17 12:02:18 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/05/17 13:04:23 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
-
-int	ft_cmd_last(t_pipex *pipex, int argc_l)
-{
-	int		i;
-	char	*ppath;
-	char	**cmd;
-	int		outfile;
-
-	i = 0;
-	cmd = ft_split(pipex->argv[argc_l - 2], ' ');
-	ft_lstadd_back(pipex->free, ft_pipex_lstnew(cmd, 2));
-	while (pipex->path[i])
-	{
-		ppath = ft_strjoin(pipex->path[i], cmd[0]);
-		if (access(ppath, X_OK) == 0)
-			break ;
-		i++;
-	}
-	ft_lstadd_back(pipex->free, ft_pipex_lstnew(ppath, 1));
-	if (pipex->here_doc == 1)
-		outfile = open(pipex->argv[1], O_APPEND | O_CREAT, 0777);
-	else
-		outfile = open(pipex->argv[1], O_TRUNC | O_CREAT, 0777);
-	dup2(outfile, STDIN_FILENO);
-	dup2(pipex->pipe1[1], STDOUT_FILENO);
-	close(outfile);
-	close(pipex->pipe1[0]);
-	execve(ppath, &cmd[1], pipex->envp);
-}
+#include "../inc/pipex.h"
 
 t_list	*ft_pipex_lstnew(void *cont, int type)
 {
@@ -53,7 +24,6 @@ t_list	*ft_pipex_lstnew(void *cont, int type)
 	test->next = NULL;
 	return (test);
 }
-
 
 int	ft_cmd_first(t_pipex *pipex, int argc_l)
 {
@@ -70,6 +40,7 @@ int	ft_cmd_first(t_pipex *pipex, int argc_l)
 		ppath = ft_strjoin(pipex->path[i], cmd[0]);
 		if (access(ppath, X_OK) == 0)
 			break ;
+		free(ppath);
 		i++;
 	}
 	ft_lstadd_back(pipex->free, ft_pipex_lstnew(ppath, 1));
@@ -82,6 +53,7 @@ int	ft_cmd_first(t_pipex *pipex, int argc_l)
 		dup2(pipex->pipe1[1], STDOUT_FILENO);
 		close(infile);
 		close(pipex->pipe1[0]);
+		close(pipex->pipe1[1]);
 		execve(ppath, &cmd[1], pipex->envp);
 	}
 }
@@ -105,14 +77,39 @@ int	ft_child(t_pipex *pipex)
 		else if (pid_n == 0)
 		{
 			pipex->argc = pipex->argc - 1;
-			ft_child_n(pipex);
+			ft_child(pipex);
 		}
 		else
 		{
 			waitpid(pid_n, NULL, 0);
-			ft_cmd_n();
+			ft_cmd_n(pipex, argc_l);
 		}
 	}
+}
+
+int	ft_cmd_n(t_pipex *pipex, int argc_l)
+{
+	int		i;
+	char	*ppath;
+	char	**cmd;
+
+	i = 0;
+	cmd = ft_split(pipex->argv[argc_l - 3], ' ');
+	ft_lstadd_back(pipex->free, ft_pipex_lstnew(cmd, 2));
+	while (pipex->path[i])
+	{
+		ppath = ft_strjoin(pipex->path[i], cmd[0]);
+		if (access(ppath, X_OK) == 0)
+			break ;
+		free(ppath);
+		i++;
+	}
+	ft_lstadd_back(pipex->free, ft_pipex_lstnew(ppath, 1));
+	dup2(pipex->pipe1[1], STDOUT_FILENO);
+	dup2(pipex->pipe1[0], STDIN_FILENO);
+	close(pipex->pipe1[0]);
+	close(pipex->pipe1[1]);
+	execve(ppath, &cmd[1], pipex->envp);
 }
 
 int	ft_parent_process(t_pipex *pipex)
@@ -130,13 +127,43 @@ int	ft_parent_process(t_pipex *pipex)
 	else if (pid_f == 0)
 	{
 		pipex->argc = pipex->argc - 2;
-		ft_child_n(pipex);
+		ft_child(pipex);
 	}
 	else
 	{
 		waitpid(pid_f, NULL, 0);
 		ft_cmd_last(pipex, argc_l);
 	}
+}
+
+int	ft_cmd_last(t_pipex *pipex, int argc_l)
+{
+	int		i;
+	char	*ppath;
+	char	**cmd;
+	int		outfile;
+
+	i = 0;
+	cmd = ft_split(pipex->argv[argc_l - 2], ' ');
+	ft_lstadd_back(pipex->free, ft_pipex_lstnew(cmd, 2));
+	while (pipex->path[i])
+	{
+		ppath = ft_strjoin(pipex->path[i], cmd[0]);
+		if (access(ppath, X_OK) == 0)
+			break ;
+		free(ppath);
+		i++;
+	}
+	ft_lstadd_back(pipex->free, ft_pipex_lstnew(ppath, 1));
+	if (pipex->here_doc == 1)
+		outfile = open(pipex->argv[argc_l - 1], O_APPEND | O_CREAT, 0777);
+	else
+		outfile = open(pipex->argv[1], O_TRUNC | O_CREAT, 0777);
+	dup2(outfile, STDOUT_FILENO);
+	dup2(pipex->pipe1[0], STDIN_FILENO);
+	close(outfile);
+	close(pipex->pipe1[0]);
+	execve(ppath, &cmd[1], pipex->envp);
 }
 
 char	**ft_grab_envp(char **envp)
