bfffdff84c3706358e52e8048f589abe86d0b988
Markus Weiss <markusweiss.mw@gmail.com>
2024-05-28 15:30:30 +0200

    new approach started. Still gathering resources

---
+++ resources/concept1.1.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   concept1.1.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 11:55:57 by maweiss           #+#    #+#             */
+/*   Updated: 2024/05/28 12:20:22 by maweiss          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+New concept.
+
+One parent process orchestrating all the childs.
+first child:
+read input file (different approach for here_doc)
+Execute command with execve (separate child for reading to pipe and execve?)
+
+
+*/
